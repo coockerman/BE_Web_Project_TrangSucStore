c06a513197b5c3c287348ef119b8d2b0306aa32d
Nguyễn Hải <haibetmen997@gmail.com>
2025-03-04 21:28:05 +0700

    update coupon

--- BEWebPNJ/obj/Debug/net8.0/BEWebPNJ.AssemblyInfo.cs
+++ BEWebPNJ/obj/Debug/net8.0/BEWebPNJ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BEWebPNJ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd29e8fdd72f6e6600ead7024f8b0a26a6640115")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44ff16eb93c13cb0f4bf7f4d31e470eca601a3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BEWebPNJ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BEWebPNJ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
